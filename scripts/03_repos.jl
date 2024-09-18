9b57db3502088d6da84a92b1c85c2e732b6f1b90
Marcelo Simas <MarceloSimas@westat.com>
2024-09-18 17:30:18 +0000

    Late commits of changes done in AWS

--- scripts/03_repos.jl
+++ scripts/03_repos.jl
@@ -2,7 +2,7 @@ using GHOST
 using GHOST: groupby
 using Distributed
 setup()
-setup_parallel(2)
+setup_parallel()
 (;conn, schema) = GHOST.PARALLELENABLER
 
 data = execute(
@@ -18,8 +18,9 @@ data = [ data[k] for k in keys(data) ];
 
 time_start = now()
 @sync @distributed for batch in data
+    @info "Running repos batch:"
+    @info batch
     find_repos(batch)
-    println(batch)
 end
 # find_repos(data[1])
 time_end = now()

fc982b0b2b5e4e84a2434a5f8bd4b87bd837c5ff
Marcelo Simas <MarceloSimas@westat.com>
2024-10-07 20:46:56 +0000

    Bigger batches of repos to collect commits for.

--- src/assets/sql/branches_min_max.sql
+++ src/assets/sql/branches_min_max.sql
@@ -3,5 +3,5 @@ SELECT branch FROM (
     FROM ghost.repos
     WHERE (status = 'Init' OR status = 'In progress') AND (commits > min_lim) AND (commits <= max_lim)
     GROUP BY branch, commits
-    ORDER BY commits LIMIT 10000
+    ORDER BY commits LIMIT 1_000_000
 ) AS unique_branches GROUP BY branch;
